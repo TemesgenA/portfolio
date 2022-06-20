33abc86fc678d391e4f626411cf4989c4868cc02
TemesgeA <temesgen.ayneka@gmail.com>
2022-06-21 00:42:21 +0300

    nav bar on progress

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
+// function toggleMobileMenu(menu) {
+//     menu.classList.toggle('open');
+// }
+
+var mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+    
+};
