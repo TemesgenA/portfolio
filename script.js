30eabbe7541983a18dec993cf4c4ca0f846434ef
TemesgeA <temesgen.ayneka@gmail.com>
2022-06-24 19:24:49 +0300

    Fixed the modal container

--- script.js
+++ script.js
@@ -185,7 +185,7 @@ modal.innerHTML = `
       <li class="language-used modal-li">javaScript</li>
     </ul>
     <hr class="modal-hr" />
-    <a href="https://ibizugbe.github.io/My-Portfolio/">
+    <a href="https://temesgena.github.io/Portfolio-mobile/">
       <button class="btn modal-btn" type="submit">
         See live
         <img
@@ -195,7 +195,7 @@ modal.innerHTML = `
         />
       </button>
     </a>
-    <a href="https://github.com/Ibizugbe/My-Portfolio">
+    <a href="https://temesgena.github.io/Portfolio-mobile/">
       <button class="btn modal-btn" type="submit">
         See Source
         <img
