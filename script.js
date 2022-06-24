318ef07cdb2a796cd4ffc028e1f3fd71498d20da
shailisharma <99400018+Jusha25@users.noreply.github.com>
2022-06-24 20:41:06 +0530

    Fixed typo error

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ contactForm.addEventListener('submit', (e) => {
   const messageList = message.replace(/[^a-zA-Z]/g, '').split('');
   for (let i = 0; i < messageList.length; i += 1) {
     if (messageList[i] === messageList[i].toUpperCase()) {
-      errorHolder.innerText = 'Error= emails must be written...    with lowercase characters.';
+      errorHolder.innerText = 'Error: emails must be written with lowercase characters.';
       e.preventDefault();
     }
   }
